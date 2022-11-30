f669757a593cbf94ff1e19fc50c6110949dba48f
joeunbiin <102141487+joeunbiin@users.noreply.github.com>
2022-11-30 09:01:54 +0900

    ver.1.0

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(".slide_group01").slick({
+    autoplay: true, // 자동재생
+    autoplaySpeed: 4000, // 간격시간
+    dots: true, // 동그라미버튼
+    pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
+    prevArrow: '<button class="prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="next"><i class="fas fa-angle-right"></i></button>',
+    responsive: [{
+        breakpoint: 1025,
+        settings: {arrows: false},
+        
+    }
+]
+})
